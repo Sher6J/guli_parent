1041b1365786ece0ec4d63a5cb5b8b2eb15d594c
Sher6J <liujx17614417627@outlook.com>
2020-07-02 20:38:28 +0800

    update

--- service/service_vod/src/main/java/cn/sher6j/vod/controller/VodController.java
+++ service/service_vod/src/main/java/cn/sher6j/vod/controller/VodController.java
@@ -1,14 +1,17 @@
 package cn.sher6j.vod.controller;
 
 import cn.sher6j.commonutils.R;
+import cn.sher6j.servicebase.exceptionhandler.GuliException;
 import cn.sher6j.vod.service.VodService;
+import cn.sher6j.vod.utils.InitVodObject;
+import cn.sher6j.vod.utils.VodConstantUtils;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -34,5 +37,28 @@ public class VodController {
     public R uploadVideoToAliyun(MultipartFile file) {
         String videoId = vodService.uploadVideoToAliyun(file);
         return R.ok().data("videoId", videoId);
+    }
+
+    /**
+     * 删除视频
+     * @param id 视频在阿里云中的id
+     * @return
+     */
+    @ApiOperation("根据视频id删除阿里云中的视频")
+    @DeleteMapping("removeVideoFromAliyun/{id}")
+    public R removeVideoFromAliyun(@PathVariable String id) {
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodObject.initVodClient(VodConstantUtils.ACCESS_KEY_ID, VodConstantUtils.ACCESS_KEY_SECRET);
+            //创建删除视频request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            //向request中设置视频id（可以传多个id）
+            request.setVideoIds(id);
+            client.getAcsResponse(request);
+            return R.ok();
+        } catch (ClientException e) {
+            e.printStackTrace();
+            throw new GuliException(20001, "删除视频失败");
+        }
     }
 }
