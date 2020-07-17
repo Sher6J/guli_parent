b00298713e11814e1ef0b4c9450253b64aa2f24a
Sher6J <liujx17614417627@outlook.com>
2020-07-17 16:43:36 +0800

    update

--- service/service_msm/src/main/java/cn/sher6j/msmservice/controller/MsmController.java
+++ service/service_msm/src/main/java/cn/sher6j/msmservice/controller/MsmController.java
@@ -5,11 +5,14 @@ import cn.sher6j.msmservice.service.MsmService;
 import cn.sher6j.msmservice.utils.RandomUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author sher6j
@@ -23,6 +26,9 @@ public class MsmController {
     @Autowired
     private MsmService msmService;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     /**
      * 发送短信
      * @param phone 目标的手机号
@@ -31,13 +37,25 @@ public class MsmController {
     @ApiOperation("发送短信的方法")
     @GetMapping("send/{phone}")
     public R sendMsm(@PathVariable String phone) {
+        //1 从redis获取验证码，如果获取到直接放回
+        String code = redisTemplate.opsForValue().get(phone);
+        if (!StringUtils.isEmpty(code)) {
+            return R.ok();
+        }
+
+        //2 若redis中获取不到，则进行阿里云发送
         //生成随机值作为验证码，将验证码传递给阿里云进行发送
-        String code = RandomUtil.getSixBitRandom();
+        code = RandomUtil.getSixBitRandom();
         Map<String, Object> param = new HashMap<>();
         param.put("code", code);
         //调用service发送短信
         boolean isSent = msmService.send(param, phone);
-        if (isSent) return R.ok();
-        else return R.error().message("短信发送失败！");
+        if (isSent) {
+            //发动成功，则将发送成功的验证码放到redis中，并设置有效时间
+            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
+            return R.ok();
+        } else {
+            return R.error().message("短信发送失败！");
+        }
     }
 }
--- service/service_msm/src/main/java/cn/sher6j/msmservice/service/impl/MsmServiceImpl.java
+++ service/service_msm/src/main/java/cn/sher6j/msmservice/service/impl/MsmServiceImpl.java
@@ -1,6 +1,15 @@
 package cn.sher6j.msmservice.service.impl;
 
 import cn.sher6j.msmservice.service.MsmService;
+import cn.sher6j.msmservice.utils.MsmConstantUtils;
+import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -22,7 +31,35 @@ public class MsmServiceImpl implements MsmService {
     @Override
     public boolean send(Map<String, Object> param, String phone) {
         if (StringUtils.isEmpty(phone)) return false;
+        String keyId = MsmConstantUtils.ACCESS_KEY_ID;
+        String secret = MsmConstantUtils.ACCESS_KEY_SECRET;
+        DefaultProfile profile = DefaultProfile.getProfile("default", keyId, secret);
+        IAcsClient client = new DefaultAcsClient(profile);
 
-        return false;
+        //设置相关参数
+        CommonRequest request = new CommonRequest();
+//        request.setMethod(MethodType.POST); //过时
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+
+        //设置发送相关的参数
+//        String signName = MsmConstantUtils.SIGN_NAME;
+//        String templateCode = MsmConstantUtils.TEMPLATE_CODE;
+        request.putQueryParameter("PhoneNumbers", phone); //手机号
+        request.putQueryParameter("SignName", "六甲在线平台"); //阿里云短信服务的签名名称
+        request.putQueryParameter("TemplateCode", "SMS_189520829"); //短信模板code
+        request.putQueryParameter("TemplateParam", JSONObject.toJSONString(param)); //要求传的是json格式，如{"code":234567}，所以用map封装
+
+        //最终发送
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            boolean isSuccess = response.getHttpResponse().isSuccess();
+            return isSuccess;
+        } catch (ClientException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
